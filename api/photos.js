23f76830ff113552fd97fbf747e37d6cd7c22b91
SamsonDeVol <devols@oregonstate.edu>
2023-06-02 14:32:07 -0700

    the turtle beats the hair unless it can't get its image data correctly formatted before the deadline

--- api/photos.js
+++ api/photos.js
@@ -3,28 +3,58 @@
  */
 
 const { Router } = require('express')
-
+const { getChannel, connectToRabbitMQ } = require('../lib/rabbitmq')
 const { validateAgainstSchema } = require('../lib/validation')
 const {
   PhotoSchema,
-  insertNewPhoto,
-  getPhotoById
+  getPhotoInfoById,
+  savePhotoInfo,
+  savePhotoFile,
+  removeUploadedFile,
+  getPhotoDownloadStreamById,
+  getPhotoById,
+  getThumbDownloadStreamById,
+  getThumbInfoById
 } = require('../models/photo')
 
 const router = Router()
 
+const photoTypes = {
+  "image/jpeg": "jpg",
+  "image/png": "png",
+}
+
+const multer = require('multer');
+const upload = multer({ dest: `${__dirname}/../uploads` });
+
+
 /*
  * POST /photos - Route to create a new photo.
  */
-router.post('/', async (req, res) => {
-  if (validateAgainstSchema(req.body, PhotoSchema)) {
+router.post('/', upload.single('photo'), async (req, res, next) => {
+  // console.log("  -- req.file:", req.file)
+  const body = JSON.parse(req.body.data)
+  // console.log("  -- req.body:", body)
+
+  if (validateAgainstSchema(body, PhotoSchema) && req.file) {
     try {
-      const id = await insertNewPhoto(req.body)
-      res.status(201).send({
+      const photo = {
+        contentType: req.file.mimetype,
+        filename: req.file.filename,
+        path: req.file.path,
+        userId: body.userId
+      }
+
+      const id = await savePhotoFile(photo)
+      const channel = getChannel();
+      // console.log("id inserted", id)
+      channel.sendToQueue('photos', Buffer.from(id.toString()));
+      await removeUploadedFile(photo)
+      res.status(200).send({
         id: id,
         links: {
           photo: `/photos/${id}`,
-          business: `/businesses/${req.body.businessId}`
+          business: `/businesses/${body.businessId}`
         }
       })
     } catch (err) {
@@ -35,7 +65,7 @@ router.post('/', async (req, res) => {
     }
   } else {
     res.status(400).send({
-      error: "Request body is not a valid photo object"
+      error: "Request body is not a valid photo object with userId"
     })
   }
 })
@@ -45,9 +75,18 @@ router.post('/', async (req, res) => {
  */
 router.get('/:id', async (req, res, next) => {
   try {
-    const photo = await getPhotoById(req.params.id)
+    const photo = await getPhotoInfoById(req.params.id)
+    // console.log(" -- photo:", photo)
     if (photo) {
-      res.status(200).send(photo)
+      delete photo.path;
+      const resBody = {
+        _id: photo._id,
+        url: `/photos/media/${photo.filename}`,
+        contentType: photo.metadata.contentType,
+        userId: photo.metadata.userId,
+        dimensions: photo.metadata.dimensions
+      }
+      res.status(200).send(resBody)
     } else {
       next()
     }
@@ -57,6 +96,60 @@ router.get('/:id', async (req, res, next) => {
       error: "Unable to fetch photo.  Please try again later."
     })
   }
+})
+
+
+router.get('/media/:id', async (req, res, next) => {
+  getPhotoDownloadStreamById(req.params.id)
+  .on('file', (file) => {
+    res.status(200).type(file.metadata.contentType);
+    console.log(file)
+  }).on('error', (err) => {
+    if (err.code === 'ENOENT') {
+      next();
+    } else {
+      next(err);
+    }
+  }).pipe(res);
+})
+
+router.get('/thumbs/:id', async (req, res, next) => {
+  try {
+    const thumb = await getThumbInfoById(req.params.id)
+    // console.log(" -- thumb:", thumb)
+    if (thumb) {
+      delete thumb.path;
+      const resBody = {
+        _id: thumb._id,
+        url: `/photos/media/thumbs/${thumb.filename}`,
+        // contentType: thumb.metadata.contentType,
+        // userId: thumb.metadata.userId,
+        // dimensions: thumb.metadata.dimensions
+      }
+      res.status(200).send(resBody)
+    } else {
+      next()
+    }
+  } catch (err) {
+    console.error(err)
+    res.status(500).send({
+      error: "Unable to fetch photo.  Please try again later."
+    })
+  }
+})
+
+router.get('/media/thumbs/:id', async (req, res, next) => {
+  getThumbDownloadStreamById(req.params.id)
+  .on('file', (file) => {
+    console.log("cont", file)
+    res.status(200).type(file.metadata.contentType);
+  }).on('error', (err) => {
+    if (err.code === 'ENOENT') {
+      next();
+    } else {
+      next(err);
+    }
+  }).pipe(res);
 })
 
 module.exports = router
--- models/photo.js
+++ models/photo.js
@@ -2,7 +2,10 @@
  * Photo schema and data accessor methods.
  */
 
-const { ObjectId } = require('mongodb')
+const { ObjectId, GridFSBucket } = require('mongodb')
+const fs = require("node:fs")
+const Jimp = require("jimp");
+const sharp = require('sharp');
 
 const { getDbReference } = require('../lib/mongo')
 const { extractValidFields } = require('../lib/validation')
@@ -11,24 +14,92 @@ const { extractValidFields } = require('../lib/validation')
  * Schema describing required/optional fields of a photo object.
  */
 const PhotoSchema = {
+  userId: { required: true },
   businessId: { required: true },
   caption: { required: false }
 }
-exports.PhotoSchema = PhotoSchema
+
+const ThumbSchema = {
+
+}
+
+// async function savePhotoInfo(photo) {
+//   const db = getDbReference()
+//   const collection = db.collection('photos')
+//   const result = await collection.insertOne(photo)
+//   return result.insertedId
+// }
+
+async function savePhotoFile(photo) {
+  console.log("=== saving photo file: ", photo)
+  return new Promise(function (resolve, reject) {
+      const db = getDbReference()
+      const bucket = new GridFSBucket(db, { bucketName: "photos" })
+      const metadata = {
+          contentType: photo.contentType,
+          userId: photo.userId
+      }
+      const uploadStream = bucket.openUploadStream(
+          photo.filename,
+          { metadata: metadata }
+      )
+      fs.createReadStream(photo.path).pipe(uploadStream)
+          .on("error", function (err) {
+              reject(err)
+          })
+          .on("finish", function (result) {
+              console.log("== write success, result:", result)
+              resolve(result._id)
+          })
+  })
+} 
+
+function removeUploadedFile(file) {
+  return new Promise((resolve, reject) => {
+    fs.unlink(file.path, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+async function getPhotoInfoById(id) {
+  console.log(" -- getting by id")
+  const db = getDbReference();
+  const bucket = new GridFSBucket(db, { bucketName: 'photos' });  
+  console.log(" -- bucket", bucket)
+  console.log(" -- valid id", ObjectId(id))
+  if (!ObjectId.isValid(id)) {
+    return null;
+  } else {
+    const results = await bucket.find({ _id: new ObjectId(id) }).toArray();
+    console.log(" -- results ", results)
+    return results[0];
+  }
+}
+
+function getPhotoDownloadStreamById(id) {
+  console.log("id", id)
+  const db = getDbReference()
+  const bucket = new GridFSBucket(db, { bucketName: "photos" })
+  return bucket.openDownloadStream(new ObjectId(id))
+}
 
 /*
  * Executes a DB query to insert a new photo into the database.  Returns
  * a Promise that resolves to the ID of the newly-created photo entry.
  */
-async function insertNewPhoto(photo) {
-  photo = extractValidFields(photo, PhotoSchema)
-  photo.businessId = ObjectId(photo.businessId)
-  const db = getDbReference()
-  const collection = db.collection('photos')
-  const result = await collection.insertOne(photo)
-  return result.insertedId
-}
-exports.insertNewPhoto = insertNewPhoto
+// async function insertNewPhoto(photo) {
+//   photo = extractValidFields(photo, PhotoSchema)
+//   photo.businessId = ObjectId(photo.businessId)
+//   const db = getDbReference()
+//   const collection = db.collection('photos')
+//   const result = await collection.insertOne(photo)
+//   return result.insertedId
+// }
 
 /*
  * Executes a DB query to fetch a single specified photo based on its ID.
@@ -38,7 +109,7 @@ exports.insertNewPhoto = insertNewPhoto
  */
 async function getPhotoById(id) {
   const db = getDbReference()
-  const collection = db.collection('photos')
+  const collection = db.collection('photos.files')
   if (!ObjectId.isValid(id)) {
     return null
   } else {
@@ -48,4 +119,126 @@ async function getPhotoById(id) {
     return results[0]
   }
 }
+
+function getDownloadStreamById(id) {
+  const db = getDbReference()
+  const bucket = new GridFSBucket(db, { bucketName: 'photos' })
+  if (!ObjectId.isValid(id)) {
+      return null
+  } else {
+      return bucket.openDownloadStream(new ObjectId(id))
+  }
+}
+
+function getThumbDownloadStreamById(id) {
+  const db = getDbReference()
+  const bucket = new GridFSBucket(db, { bucketName: 'thumbs'})
+  return bucket.openDownloadStream(new ObjectId(id))
+}
+
+async function saveThumbFile(thumb) {
+  console.log(" == saving thumb", thumb)
+  return new Promise(function (resolve, reject) {
+      const db = getDbReference()
+      const bucket = new GridFSBucket(db, { bucketName: "thumbs" })
+      const metadata = {
+          contentType: "image/jpeg",
+      }
+      const uploadStream = bucket.openUploadStream(
+          thumb.filename,
+          { metadata: metadata }
+      )
+      fs.createReadStream(thumb.path).pipe(uploadStream)
+          .on("error", function (err) {
+              reject(err)
+          })
+          .on("finish", function (result) {
+              console.log("== write success, result:", result)
+              resolve(result._id)
+          })
+  })
+} 
+
+// console.log(photo)
+// return new Promise(function (resolve, reject) {
+//     const db = getDbReference()
+//     const bucket = new GridFSBucket(db, { bucketName: "photos" })
+//     const metadata = {
+//         contentType: photo.contentType,
+//         userId: photo.userId
+//     }
+//     const uploadStream = bucket.openUploadStream(
+//         photo.filename,
+//         { metadata: metadata }
+//     )
+//     fs.createReadStream(photo.path).pipe(uploadStream)
+//         .on("error", function (err) {
+//             reject(err)
+//         })
+//         .on("finish", function (result) {
+//             console.log("== write success, result:", result)
+//             resolve(result._id)
+//         })
+// })
+// } 
+
+
+async function resizePhoto(photo) {
+  console.log("photo dimensions", photo)
+  const thumb = await Jimp.read(photo);
+  await thumb.resize(100,100)
+  console.log(photo)
+  return thumb
+}
+
+async function updatePhotoSizeById(id, dimensions) {
+  const db = getDbReference()
+  const collection = db.collection('photos.files')
+
+  if (!ObjectId.isValid(id)) {
+      return null
+  } else {
+      const result = await collection.updateOne(
+          { _id: new ObjectId(id) },
+          { $set: { "metadata.dimensions": dimensions }}
+      )
+      return result.matchedCount > 0
+  }
+}
+
+async function createThumbnail(id, photoData) {
+  console.log("pd", photoData)
+  const thumb = sharp(photoData).resize(100,100)
+  const db = getDbReference()
+  const photo = await getPhotoDownloadStreamByFilename(id)
+  console.log(" -- photo", photo)
+  // const thumb = await resizePhoto(dimensions)
+  // console.log(" -- thumber", thumb)s
+  await saveThumbFile(thumb)
+}
+
+async function getThumbInfoById(id) {
+  const db = getDbReference();
+  const bucket = new GridFSBucket(db, { bucketName: 'thumbs' });  
+  if (!ObjectId.isValid(id)) {
+    return null;
+  } else {
+    const results = await bucket.find({ _id: new ObjectId(id) }).toArray();
+    return results[0];
+  }
+}
+
+exports.getThumbInfoById = getThumbInfoById
+exports.PhotoSchema = PhotoSchema
+// exports.savePhotoInfo = savePhotoInfo
+exports.savePhotoFile = savePhotoFile
+exports.removeUploadedFile = removeUploadedFile
+exports.getPhotoInfoById = getPhotoInfoById
+// exports.insertNewPhoto = insertNewPhoto
 exports.getPhotoById = getPhotoById
+exports.getPhotoDownloadStreamById = getPhotoDownloadStreamById
+exports.getDownloadStreamById = getDownloadStreamById
+exports.createThumbnail = createThumbnail
+exports.updatePhotoSizeById = updatePhotoSizeById
+exports.saveThumbFile = saveThumbFile
+exports.getThumbDownloadStreamById = getThumbDownloadStreamById
